96b4c6515212d03cca6ceecee5eb913e604c4e5d
Andres Garcia <andres.fgp@hotmail.com>
2022-10-05 13:27:26 -0500

    helmet component

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Products from './components/products/Products';
 import initialState from '../../initialState';
+import MetaHead from '../../components/meta-head/MetaHead';
 
 export const Home = () => {
-  return <Products products={initialState.products} />;
+  const title = "E-commerce Store"
+  const description = "Super Store"
+  const image = "https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+  return (
+  <>
+    <MetaHead title={title} description={description} image={image} />
+    <Products products={initialState.products} />
+  </>
+  );
 };
 
 export default Home;
